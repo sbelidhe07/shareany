0090b11f313310a85e2fc8862379a376ff8bf5a3
sbelidhe <sbelidhe@gmail.com>
2021-03-10 02:54:11 +0530

    Modified and added files

--- shareany_service.py
+++ shareany_service.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
+from flask import Flask, render_template, url_for,request, session, redirect
 import bcrypt
 
 app = Flask(__name__)
@@ -9,7 +9,6 @@ app.config[
     'MONGO_URI'] = 'mongodb+srv://portalappuser:admin123@cluster0.89wan.mongodb.net/shareany?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true'
 
 mongo = PyMongo(app)
-
 
 
 @app.route('/index')
@@ -27,19 +26,19 @@ def index():
 def dashboarda():
     if 'username' in session:
         if session['role'] == 'Admin':
-           return render_template('dashboard_admin.html');
+          return render_template('dashboard_admin.html');
         return render_template('index.html')
 
 @app.route('/tracking')
 def tracking():
     if 'username' in session:
-        return render_template('tracking.html');
+       return render_template('tracking.html');
     return render_template('index.html')
 
-@app.route('/request')
-def request():
+@app.route('/requestf')
+def requestf():
     if 'username' in session:
-        return render_template('request.html');
+      return render_template('request.html');
     return render_template('index.html')
 
 @app.route('/logout')
@@ -50,62 +49,74 @@ def logout():
 @app.route('/dashboardo')
 def dashboardo():
     if 'username' in session:
-        return render_template('dashboard_other.html');
+       return render_template('dashboard_other.html');
     return render_template('index.html')
 
 @app.route('/menubara')
 def menubara():
     if 'username' in session:
-        return render_template('menubar_admin.html');
+      return render_template('menubar_admin.html');
     return render_template('index.html')
 
 @app.route('/logisticsa')
 def logisticsa():
     if 'username' in session:
-        return render_template('logistics_add.html');
+      return render_template('logistics_add.html');
+    return render_template('index.html')
+
+@app.route('/mplans')
+def mplans():
+    if 'username' in session:
+      return render_template('mplans.html');
     return render_template('index.html')
 
 @app.route('/categorya')
 def categorya():
     if 'username' in session:
-        return render_template('category_add.html');
+      return render_template('category_add.html');
     return render_template('index.html')
 
 @app.route('/gifta')
 def gifta():
     if 'username' in session:
-        return render_template('gift_add.html');
+      return render_template('gift_add.html');
     return render_template('index.html')
 
 @app.route('/clienta')
 def clienta():
     if 'username' in session:
-        return render_template('client_add.html');
+      return render_template('client_add.html');
     return render_template('index.html')
 
 @app.route('/menubar')
 def menubar():
     if 'username' in session:
-        return render_template('menubar.html');
+      return render_template('menubar.html');
     return render_template('index.html')
 
 @app.route('/support')
 def support():
     if 'username' in session:
-        return render_template('support.html');
+      return render_template('support.html');
+    return render_template('index.html')
+
+@app.route('/donate')
+def donate():
+    if 'username' in session:
+      return render_template('donate.html');
     return render_template('index.html')
 
 @app.route('/notifications')
 def notifications():
     if 'username' in session:
-        return render_template('notifications.html');
+      return render_template('notifications.html');
     return render_template('index.html')
 
 
 @app.route('/login', methods=['POST'])
 def login():
     users = mongo.db.users
-    print(request.form['role']);
+    print(request.form['role'])
     login_user = users.find_one({'username': request.form['username'], 'role': request.form['role']})
     if login_user:
            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'),bcrypt.gensalt())
